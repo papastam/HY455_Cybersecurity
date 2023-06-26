d04b13fa4eeab595d62ea557c5a9eb834a52709a
Chris Papastamos <xristos.papastamos@gmail.com>
2023-06-26 21:18:46 +0300

    final assignment7 submit

--- assignment7/assignemnt7.py
+++ assignment7/assignemnt7.py
@@ -1,10 +1,11 @@
 from NVD_to_mySQL import import_NVDs
 from exploitdb_to_mySQL import import_from_explotdb
 from programs_to_mySQL import import_host_programs
-from database_functions import *
+from util_functions import *
 import mysql.connector
 from time import sleep
 import datetime
+from tabulate import tabulate
 
 db_config = {
     'host': 'localhost',
@@ -16,29 +17,78 @@ db_config = {
 # Connect to the database
 conn = mysql.connector.connect(**db_config)
 
+def main_menu():
+    # The menu for the user to choose what to import
+    while True:
+        print("\n\033[96mWhat would you like to do?")
+        print("      ------ Imports ------")
+        print("\033[00m\033[46m1.\033[00m\033[96m Import all CVEs from NVD database (this may take a while)")
+        print("\033[00m\033[46m2.\033[00m\033[96m Import everything from ExploitDB")
+        print("\033[00m\033[46m3.\033[00m\033[96m Import all Host Programs")
+        print("\033[00m\033[46m4.\033[00m\033[96m Import all previous")
+        print("     ------ DB options ------")
+        print("\033[00m\033[46m5.\033[00m\033[96m Display Database Statistics")
+        print("\033[00m\033[46m6.\033[00m\033[96m Drop all tables")
+        print("     -------- Report --------")
+        print("\033[00m\033[46m7.\033[00m\033[96m Queries")
+        print("\033[00m\033[46m8.\033[00m\033[96m Generate Report for Vaunerable Installed Programs")
+        print()
+        print("\033[00m\033[46m9.\033[00m\033[96m Exit")
+        print()
+        choice = input("Enter your choice: ")
+        print("\033[00m")
+
+        if choice == '1':
+            import_NVDs(conn)
+        elif choice == '2':
+            import_from_explotdb(conn)
+        elif choice == '3':
+            import_host_programs(conn)
+        elif choice == '4':
+            import_NVDs(conn,start_date=datetime.datetime(2022,1,1))
+            import_from_explotdb(conn)
+            import_host_programs(conn)
+        elif choice == '5':
+            display_stats(conn)
+        elif choice == '6':
+            clear_db(conn)
+        elif choice == '7':
+            query_db()
+        elif choice == '8':
+            generate_report(conn)
+        elif choice == '9':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+        sleep(1)
+
+    print("Goodbye!")
+    conn.close()
+
 def query_db():
     #Qeries menu
     while True:
-        print("\n\033[96mWhat would you like to query?")
+        print("\n\033[34mWhat would you like to query?")
         print("      ------ CVEs ------")
-        print("\033[00m\033[46m1.\033[00m\033[96m CVEs by CVE ID")
-        print("\033[00m\033[46m2.\033[00m\033[96m CVEs by CWE ID")
-        print("\033[00m\033[46m3.\033[00m\033[96m CVEs by CVSScore")
-        print("\033[00m\033[46m4.\033[00m\033[96m CVEs by product")
-        print("\033[00m\033[46m5.\033[00m\033[96m CVEs by published date")
+        print("\033[00m\033[44m1.\033[00m\033[34m CVEs by CVE ID")
+        print("\033[00m\033[44m2.\033[00m\033[34m CVEs by CWE ID")
+        print("\033[00m\033[44m3.\033[00m\033[34m CVEs by CVSScore")
+        print("\033[00m\033[44m4.\033[00m\033[34m CVEs by product")
+        print("\033[00m\033[44m5.\033[00m\033[34m CVEs by published date")
         print("      ------ Installed Software ------")
-        print("\033[00m\033[46m6.\033[00m\033[96m Installed Software")
-        print("\033[00m\033[46m7.\033[00m\033[96m Installed Software by installed date")
+        print("\033[00m\033[44m6.\033[00m\033[34m Installed Software")
+        print("\033[00m\033[44m7.\033[00m\033[34m Installed Software by installed date")
         print()
-        print("\033[00m\033[46m8.\033[00m\033[96m Back")
+        print("\033[00m\033[44m8.\033[00m\033[34m Back")
         print()
 
-        choice = input("\033[96mEnter your choice:")
+        choice = input("\033[34mEnter your choice:")
         if choice == '1':
             cwe_id = input("\033[96mEnter CVE ID:")
             print(f"\033[00m\033[46mQuery results for {cwe_id}:\033[00m")
             list = query_cve(cwe_id)
-            display_cve_list(list,method="full")
+            display_cve(list,method="full")
         elif choice == '2':
             cwe_id = input("\033[96mEnter CWE ID:")
             print(f"\033[00m\033[46mQuery results for {cwe_id}:\033[00m")
@@ -47,7 +97,8 @@ def query_db():
         elif choice == '3':
             cvsscore = input("\033[96mEnter CVSScore:")
             print(f"\033[00m\033[46mQuery results for {cvsscore}:\033[00m")
-            print(query_cvsscore(cvsscore))
+            list = query_cvsscore(conn, cvsscore)
+            display_cve_list(list)
         elif choice == '4':
             product = input("\033[96mEnter product:")
             print(f"\033[00m\033[46mQuery results for {product}:\033[00m")
@@ -60,7 +111,11 @@ def query_db():
             list, count = query_published_date(conn, published_date_start, published_date_end)
             display_cve_list(list)
         elif choice == '6':
-            print(installed_software(conn))
+            list,count = query_installed_software(conn)
+            
+            print("\033[35m")
+            print(tabulate(list, headers=["Program Name","Program Version"], tablefmt="pretty"))
+            print("\033[00m")
         elif choice == '7':
             installed_date = input("\033[96mEnter installed date:")
             print(f"\033[00m\033[46mQuery results for {installed_date}:\033[00m")
@@ -72,51 +127,5 @@ def query_db():
 
         sleep(1)
 
-# The menu for the user to choose what to import
-while True:
-    print("\n\033[96mWhat would you like to do?")
-    print("      ------ Imports ------")
-    print("\033[00m\033[46m1.\033[00m\033[96m Import all CVEs from NVD database (this may take a while)")
-    print("\033[00m\033[46m2.\033[00m\033[96m Import everything from ExploitDB")
-    print("\033[00m\033[46m3.\033[00m\033[96m Import all Host Programs")
-    print("\033[00m\033[46m4.\033[00m\033[96m Import all previous")
-    print("     ------ DB options ------")
-    print("\033[00m\033[46m5.\033[00m\033[96m Display Database Statistics")
-    print("\033[00m\033[46m6.\033[00m\033[96m Drop all tables")
-    print("\033[00m\033[46m7.\033[00m\033[96m Query Database")
-    print()
-    print("     -------- Report --------")
-    print("\033[00m\033[46m8.\033[00m\033[96m Generate Report for Vaunerable Installed Programs")
-    print()
-    print("\033[00m\033[46m9.\033[00m\033[96m Exit")
-    print()
-    choice = input("Enter your choice: ")
-    print("\033[00m")
-
-    if choice == '1':
-        import_NVDs(conn)
-    elif choice == '2':
-        import_from_explotdb(conn)
-    elif choice == '3':
-        import_host_programs(conn)
-    elif choice == '4':
-        import_NVDs(conn,start_date=datetime.datetime(2022,1,1))
-        import_from_explotdb(conn)
-        import_host_programs(conn)
-    elif choice == '5':
-        display_stats(conn)
-    elif choice == '6':
-        clear_db(conn)
-    elif choice == '7':
-        query_db()
-    elif choice == '8':
-        generate_report()
-    elif choice == '9':
-        break
-    else:
-        print("Invalid choice. Please try again.")
-
-    sleep(2)
-
-print("Goodbye!")
-conn.close()
+if __name__ == "__main__":
+    main_menu()
--- assignment7/util_functions.py
+++ assignment7/util_functions.py
@@ -4,6 +4,18 @@ from datetime import datetime, timedelta
 
 nvd_api_url = 'https://services.nvd.nist.gov/rest/json/cves/2.0'
 
+def colorized_cvss(score):
+    if score >= 9:
+        return "\033[00m\033[31m" + str(score) + "\033[00m"
+    elif score >= 7:
+        return "\033[00m\033[91m" + str(score) + "\033[00m"
+    elif score >= 4:
+        return "\033[00m\033[33m" + str(score) + "\033[00m"
+    elif score >= 1:
+        return "\033[00m\033[32m" + str(score) + "\033[00m"
+    else:
+        return "\033[00m\033[34m" + str(score) + "\033[00m"
+
 def display_cve_list(cve_list, method="short"):
     if method != "short" and method != "long" and method != "full":
         print("Invalid method. Please use 'short' or 'long'.")
@@ -12,6 +24,8 @@ def display_cve_list(cve_list, method="short"):
     for cve in cve_list:
         if type(cve) == dict:
             display_cve(cve, method)
+        elif type(cve) == tuple:
+            display_cve({"id": cve[0],"sourceIdentifier": cve[1], 'published': cve[2], 'last_modified': cve[3], "descriptions": [{"value": cve[4]}],'metrics': {'cvssMetricV31':[{'cvssData':{'baseScore':cve[5]}}]} , 'weaknesses': cve[6], 'configurations': cve[7] }, method)
         else:
             try:
                 display_cve(cve_list[cve], method)
@@ -20,7 +34,7 @@ def display_cve_list(cve_list, method="short"):
                 print(f"\033[00m\033[41m{cve_list}\033[00m")
                 return
         count += 1
-    print(f"\033[00m\033[46mTotal CVEs: {count}\033[00m")
+    print(f"\n\033[00m\033[46mTotal CVEs: {count}\033[00m")
 
 def display_cve(cve,method="short"):
     if "cve" in cve:
@@ -35,7 +49,7 @@ def display_cve(cve,method="short"):
             base_score = None
     try:
         if method == "short":
-            print(f"\033[00m\033[45m{cve['id']}\033[00m\n")
+            print(f"\033[00m\033[45m{cve['id']}\033[00m, ", end="")
         elif method == "long":
             print(f"\033[00m\033[45m{cve['id']}\033[00m\033[95m {cve['descriptions'][0]['value']}\n")
         elif method == "full":
@@ -43,7 +57,7 @@ def display_cve(cve,method="short"):
             print(f"\033[04mSource Identifier:\033[00m\033[95m {cve['sourceIdentifier']}")
             print(f"\033[04mPublished:\033[00m\033[95m {cve['published']}")
             print(f"\033[04mLast Modified:\033[00m\033[95m {cve['lastModified']}")
-            print(f"\033[04mBase Score:\033[00m\033[95m {base_score}")
+            print(f"\033[04mBase Score:\033[00m\033[95m {colorized_cvss(base_score)}\033[95m")
             print(f"\033[04mWeaknesses:\033[00m\033[95m {cve['weaknesses']}")
             print(f"\033[04mConfigurations:\033[00m\033[95m {cve['configurations']}")
             print()
@@ -142,35 +156,15 @@ def query_cwe(cwe_id):
         
     return result, count
 
-def query_cvsscore(cvsscore):
-    result = []
-    done = False
-    startindex = 0
-    count = 0 
-    failcount=0
+def query_cvsscore(conn, cvsscore):
+    #Query the database for all CVEs with a CVSS score greater than or equal to the given score
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM cves WHERE base_score >= %s', (cvsscore,))
+    result = cursor.fetchall()
+    cursor.close()
+    return result
 
-    while not done:
-        response = requests.get(f'{nvd_api_url}?cvssV3Severity={cvsscore}&startIndex={2000*startindex}&resultsPerPage=2000')
-
-        if response.status_code == 200:
-            data = response.json()
-            result += data["vulnerabilities"]
-            count += data["resultsPerPage"]
-
-            if data["resultsPerPage"] + data['startIndex'] < data["totalResults"]:
-                startindex += 1
-            else:
-                done = True
-        else:
-            if failcount >=3:
-                break
-
-            failcount += 1
-            print(f"API call error: {response.headers['message']}")
-
-    return result, count
-
-def query_product(product):
+def query_product(product, version=None):
     result = []
     done = False
     startindex = 0
@@ -178,7 +172,10 @@ def query_product(product):
     failcount=0
 
     # create CPE from vendor
-    cpe = f"cpe:2.3:a:*:{product}"
+    if version:
+        cpe = f"cpe:2.3:*:*:{product}:{version}"
+    else:
+        cpe = f"cpe:2.3:*:*:{product}"
 
     while (not done):
         response = requests.get(f'{nvd_api_url}?virtualMatchString={cpe}&startIndex={2000*startindex}&resultsPerPage=2000')
@@ -250,10 +247,11 @@ def query_published_date(conn, published_date_start, published_date_end):
 
 def query_installed_software(conn):
     cursor = conn.cursor()
-    cursor.execute(f'SELECT * FROM installed_programs')
+    cursor.execute(f'SELECT program_name,program_version FROM installed_programs')
     result = cursor.fetchall()
+
     cursor.close()
-    return result
+    return result, len(result)
 
 def query_installed_date(conn, installed_date):
     cursor = conn.cursor()
@@ -262,16 +260,40 @@ def query_installed_date(conn, installed_date):
     cursor.close()
     return result
 
-def installed_software(conn):
-    cursor = conn.cursor()
-    cursor.execute(f'SELECT * FROM installed_programs')
-    result = cursor.fetchall()
-    cursor.close()
-    return result
-
 def generate_report(conn):
-    cursor = conn.cursor()
-    cursor.execute(f'SELECT * FROM cves')
-    result = cursor.fetchall()
-    cursor.close()
-    return result
+    programs_list, pcnt = query_installed_software(conn)
+    print(f"\033[35mFound {pcnt} programs installed on this system\033[00m")
+    vaulnerable_programs = []
+
+    for program in programs_list:
+        print(f"\033[33mQuerying NVD for \033[00m\033[43m{program[0]}\033[00m\033[33m version \033[00m\033[43m{program[1]}\033[00m")
+        result, count = query_product(program[0], program[1].split('+')[0].split('~')[0])
+        if count == 0:
+            print(f"\033[32mThere are no CVEs for {program[0]} version {program[1]}\033[00m")
+        else:
+            print(f"\033[32mFound {count} CVEs for {program[0]} version {program[1]}:\033[41m")
+            display_cve_list(result, method="long")
+
+            vaulnerable_programs.append([program[0], count, result])
+
+    if len(vaulnerable_programs) > 0:
+        # display report
+        print(f"\033[31mFound {len(vaulnerable_programs)} vulnerable programs\033[00m")
+        print(f"\033[31mReport:\033[00m")
+        for program in vaulnerable_programs:
+            print(f"\n{program[0]} is associated with {program[1]} CVEs: ")
+            display_cve_list(program[2], method="full")
+
+        # print report file
+        report_file = open(f"report_{datetime.now()}.txt", "w")
+        report_file.write("Vulnerable Programs:\n")
+        for program in vaulnerable_programs:
+            report_file.write(f"{program[0]} is associated with {program[1]} CVEs: \n")
+            for cve in program[2]:
+                cve = cve['cve']
+                report_file.write(f"|{cve['id']}| {cve['descriptions'][0]['value']}\n")
+        print(f"\033[31mReport saved to {report_file.name}\033[00m")
+        report_file.close()
+
+    else:
+        print(f"\033[32mNo vulnerable programs found\033[00m")
